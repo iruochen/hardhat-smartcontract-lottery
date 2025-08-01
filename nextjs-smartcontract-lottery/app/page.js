850d3207accac72028ed7630264c01d9a98309dd
ruochen <wangrui@ruochen.email>
2025-08-01 08:43:59 +0800

    feat(frontend): integrate getEntranceFee and enterRaffle contract methods

--- nextjs-smartcontract-lottery/app/page.js
+++ nextjs-smartcontract-lottery/app/page.js
@@ -10,7 +10,6 @@ export default function Home() {
 		<MoralisProvider initializeOnMount={false}>
 			<main className="bg-violet-100 min-h-screen ">
 				<Header />
-				321321
 				<LotteryEntrance />
 			</main>
 		</MoralisProvider>
--- nextjs-smartcontract-lottery/components/LotteryEntrance.js
+++ nextjs-smartcontract-lottery/components/LotteryEntrance.js
@@ -1,11 +1,58 @@
 import { useWeb3Contract } from "react-moralis"
+import { abi, contractAddress } from "@/constants"
+import { useMoralis } from "react-moralis"
+import { useEffect, useState } from "react"
+import { ethers } from "ethers"
 
 export default function LotteryEntrance() {
-	// const { runContractFunction: enterRaffle } = useWeb3Contract({
-	// 	abi: ,
-	// 	contractAddress: ,
-	// 	functionName: ,
-	// 	params: {}
-	// })
-	return <div>Hi from lottery entrance!</div>
+	const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+	const chainId = parseInt(chainIdHex)
+	const [entranceFee, setEntranceFee] = useState("0")
+
+	const raffleAddress =
+		chainId in contractAddress ? contractAddress[chainId][0] : null
+
+	const { runContractFunction: enterRaffle } = useWeb3Contract({
+		abi: abi,
+		contractAddress: raffleAddress,
+		functionName: "enterRaffle",
+		params: {},
+		msgValue: entranceFee,
+	})
+
+	const { runContractFunction: getEntranceFee } = useWeb3Contract({
+		abi: abi,
+		contractAddress: raffleAddress,
+		functionName: "getEntranceFee",
+		params: {},
+	})
+
+	useEffect(() => {
+		if (isWeb3Enabled) {
+			async function updateUI() {
+				const entranceFeeFromCall = await getEntranceFee()
+				setEntranceFee(entranceFeeFromCall)
+			}
+			updateUI()
+		}
+	}, [isWeb3Enabled])
+	return (
+		<div>
+			Hi from lottery entrance!
+			{raffleAddress ? (
+				<div>
+					<button
+						onClick={() => {
+							enterRaffle()
+						}}
+					>
+						Enter Raffle
+					</button>
+					Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH
+				</div>
+			) : (
+				<div>No Raffle Address Detected</div>
+			)}
+		</div>
+	)
 }
